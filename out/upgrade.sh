2f9f3e5f9cf3f778068e8d7a7a177083d20bb6ca
Du <duhaiwen@gmail.com>
2020-06-23 18:04:52 +0800

    upgrade

--- out/upgrade.sh
+++ out/upgrade.sh
@@ -4,31 +4,58 @@ export CORE_PEER_LOCALMSPID=Org1MSP
 export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 export CORE_PEER_ADDRESS=peer0.org1.example.com:21000
-peer chaincode install -n mycc -v 1.1 -l golang -p github.com/chaincode/chaincode_example02/go/
+peer chaincode install -n mycc -v 1.3 -l golang -p github.com/chaincode/chaincode_example02/go/
 export CORE_PEER_LOCALMSPID=Org2MSP
 export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
 export CORE_PEER_ADDRESS=peer0.org2.example.com:22000
-peer chaincode install -n mycc -v 1.1 -l golang -p github.com/chaincode/chaincode_example02/go/
+peer chaincode install -n mycc -v 1.3 -l golang -p github.com/chaincode/chaincode_example02/go/
 export CORE_PEER_LOCALMSPID=Org3MSP
 export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.example.com/peers/peer0.org3.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org3.example.com/users/Admin@org3.example.com/msp
 export CORE_PEER_ADDRESS=peer0.org3.example.com:23000
-peer chaincode install -n mycc -v 1.1 -l golang -p github.com/chaincode/chaincode_example02/go/
+peer chaincode install -n mycc -v 1.3 -l golang -p github.com/chaincode/chaincode_example02/go/
 export CORE_PEER_LOCALMSPID=Org4MSP
 export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org4.example.com/peers/peer0.org4.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org4.example.com/users/Admin@org4.example.com/msp
 export CORE_PEER_ADDRESS=peer0.org4.example.com:24000
-peer chaincode install -n mycc -v 1.1 -l golang -p github.com/chaincode/chaincode_example02/go/
+peer chaincode install -n mycc -v 1.3 -l golang -p github.com/chaincode/chaincode_example02/go/
 export CORE_PEER_LOCALMSPID=Org5MSP
 export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org5.example.com/peers/peer0.org5.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org5.example.com/users/Admin@org5.example.com/msp
 export CORE_PEER_ADDRESS=peer0.org5.example.com:25000
-peer chaincode install -n mycc -v 1.1 -l golang -p github.com/chaincode/chaincode_example02/go/
+peer chaincode install -n mycc -v 1.3 -l golang -p github.com/chaincode/chaincode_example02/go/
 export CORE_PEER_LOCALMSPID=Org6MSP
 export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org6.example.com/peers/peer0.org6.example.com/tls/ca.crt
 export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org6.example.com/users/Admin@org6.example.com/msp
 export CORE_PEER_ADDRESS=peer0.org6.example.com:26000
-peer chaincode install -n mycc -v 1.1 -l golang -p github.com/chaincode/chaincode_example02/go/
+peer chaincode install -n mycc -v 1.3 -l golang -p github.com/chaincode/chaincode_example02/go/
 
-peer chaincode upgrade -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C mychannel -n mycc -v 1.1 -c '{"Args":["init","a","100","b","200","c","300"]}' -P "AND ('Org1MSP.peer','Org2MSP.peer')"
+peer chaincode upgrade -o orderer.example.com:7050 --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem  -C mychannel -n mycc -v 1.3 -c '{"Args":["init","a","100","b", "200"]}' -P "AND ('Org1MSP.peer','Org2MSP.peer')"
+
+
+#=================================================================================================
+
+export CORE_PEER_LOCALMSPID=Org1MSP
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer1.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=peer1.org1.example.com:21001
+peer chaincode install -n mycc -v 1.3 -l golang -p github.com/chaincode/chaincode_example02/go/
+export CORE_PEER_LOCALMSPID=Org1MSP
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/peers/peer2.org1.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
+export CORE_PEER_ADDRESS=peer2.org1.example.com:21002
+peer chaincode install -n mycc -v 1.3 -l golang -p github.com/chaincode/chaincode_example02/go/
+
+
+export CORE_PEER_LOCALMSPID=Org2MSP
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer1.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=peer1.org2.example.com:22001
+peer chaincode install -n mycc -v 1.3 -l golang -p github.com/chaincode/chaincode_example02/go/
+export CORE_PEER_LOCALMSPID=Org2MSP
+export CORE_PEER_TLS_ROOTCERT_FILE=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/peers/peer2.org2.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+export CORE_PEER_ADDRESS=peer2.org2.example.com:22002
+peer chaincode install -n mycc -v 1.3 -l golang -p github.com/chaincode/chaincode_example02/go/
+
